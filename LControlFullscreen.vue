e9fe88110ce906e67e2978f1672245f5228f6eb6
Michael Underwood <mike.underwood@gmail.com>
2020-08-14 17:01:05 -0600

    Initial commit

---
+++ LControlFullscreen.vue
@@ -0,0 +1,24 @@
+<script>
+import { OptionsMixin, ControlMixin, propsBinder, optionsMerger } from 'vue2-leaflet';
+import LeafletFullscreen from 'leaflet-fullscreen';
+import 'leaflet-fullscreen/dist/leaflet.fullscreen.css';
+export default {
+  name: 'LControlFullscreen',
+  mixins: [
+    ControlMixin,
+    OptionsMixin,
+  ],
+  mounted () {
+    const options = optionsMerger({
+      ...this.controlOptions,
+      options: this.options,
+    }, this);
+    this.mapObject = new L.Control.Fullscreen(options);
+    propsBinder(this, this.mapObject, this.$options.props);
+    this.mapObject.addTo(this.$parent.mapObject);
+  },
+  render () {
+    return null;
+  },
+};
+</script>
---
+++ example.vue
@@ -0,0 +1,40 @@
+<template>
+  <div style="height: 100%; width: 100%">
+    <l-map
+      style="height: 400px"
+      :center="mapCentre"
+      :zoom="mapZoom"
+    >
+      <l-tile-layer
+        :url="tileUrl"
+        :attribution="tileAttribution"
+      />
+      <l-control-fullscreen position="topleft" />
+    </l-map>
+  </div>
+</template>
+
+<script>
+import L from 'leaflet';
+import { LMap, LTileLayer } from 'vue2-leaflet';
+import LControlFullscreen from './LControlFullscreen.vue';
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LControlFullscreen,
+  },
+  data () {
+    return {
+      mapCentre: [51, -114],
+      mapZoom: 10,
+      tileUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      tileAttribution: '&copy; <a href="//osm.org/copyright">OpenStreetMap</a> contributors',
+    };
+  },
+};
+</script>
+
+<style>
+@import '~leaflet/dist/leaflet.css';
+</style>
